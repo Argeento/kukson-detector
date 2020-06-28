f0b1ed9c220350641e90808fcfd83b3158e03a2e
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2020-06-28 20:45:56 +0200

    Init

---
+++ script.js
@@ -0,0 +1,37 @@
+const predictionEl = document.querySelector('#prediction')
+let model, webcam, maxPredictions
+
+async function init() {
+  const modelURL = '/model/model.json'
+  const metadataURL = '/model/metadata.json'
+
+  model = await tmImage.load(modelURL, metadataURL)
+  maxPredictions = model.getTotalClasses()
+
+  webcam = new tmImage.Webcam(200, 200, false) // width, height, flip
+  await webcam.setup()
+  await webcam.play()
+  window.requestAnimationFrame(loop)
+
+  document.getElementById('webcam-container').appendChild(webcam.canvas)
+}
+
+async function loop() {
+  webcam.update() // update the webcam frame
+  await predict()
+  window.requestAnimationFrame(loop)
+}
+
+// run the webcam image through the image model
+async function predict() {
+  const predictions = await model.predict(webcam.canvas)
+  predictions.sort((a, b) => a.probability - b.probability < 0 ? 1 : -1)
+  const prediction = predictions[0]
+
+  predictionEl.textContent = prediction.probability > 0.7
+    ? `${prediction.className} (${prediction.probability * 100 >> 0}%)`
+    : 'Not a cat'
+}
+
+// window.addEventListener('click', () => init())
+init()
